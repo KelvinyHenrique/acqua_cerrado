b8f57c99caaafae04e2401c73a063f5b26ffdd30
Kelviny Henrique <kelvinyhenrique17@gmail.com>
2019-12-15 14:32:22 -0300

    teste header

--- php/comprar.php
+++ php/comprar.php
@@ -11,6 +11,7 @@ $assunto = $_POST['subject'];
 $plano = $_POST['planos'];
 $endereco = $_POST['endereco'];
 $fpagamento = $_POST['fpagamento'];
+$mensagem = $_POST['message'];
 
 echo "Email: ".$email."<br/>"."Nome: ".$nome."<br/>"."Telefone: ".$telefone."<br/>"."Assunto: 
 ".$assunto."<br/>"."Plano: ".$plano."<br/>"."Endereço: ".$endereco."<br/>"."Forma de Pagamento: ".$fpagamento;
@@ -18,11 +19,10 @@ echo "Email: ".$email."<br/>"."Nome: ".$nome."<br/>"."Telefone: ".$telefone."<br
 $to = "contato@acquacerrado.com.br"; 
 
 $message = "Email: ".$email."<br/>"."Nome: ".$nome."<br/>"."Telefone: ".$telefone."<br/>"."Assunto: 
-".$assunto."<br/>"."Plano: ".$plano."<br/>"."Endereço: ".$endereco."<br/>"."Forma de Pagamento: ".$fpagamento;
+".$assunto."<br/>"."Mensagem: "$mensagem."<br/>"."Plano: ".$plano."<br/>"."Endereço: ".$endereco."<br/>"."Forma de Pagamento: ".$fpagamento;
 
 
-$headers = "De:". $email;
-$headers = 'Content-type: text/html;' . "\r\n";
+$headers = 'Content-type: text/html;' . "\r\n".'From: '.$site_title.' <'.$user_email.'>' . PHP_EOL .
 
 mail($to, $assunto, $message, $headers);
 
