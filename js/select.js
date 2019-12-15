adbd55e62d67681a471eddff35a093622c0fd5a2
Kelviny Henrique <kelvinyhenrique17@gmail.com>
2019-12-15 14:53:21 -0300

    Segurança

--- js/select.js
+++ js/select.js
@@ -12,10 +12,8 @@ $(".select-basico").on('change', function (e) {
         case '1':
             if (checkbox == 'false') {
                 $('.valor-plano-basico').html('R$ 419');
-                $(`.botao-comprar-basico`).attr('href', 'https://www.mercadopago.com.br/checkout/v1/redirect/66dbc52d-6efc-42c5-8e05-d33ef1619e16/express/?preference-id=192393632-6c833960-c398-4aee-91b0-65daa3a25ba8');
             } else {
                 $('.valor-plano-basico').html('R$ 499');
-                $(`.botao-comprar-basico`).attr('href', 'https://www.mercadopago.com.br/checkout/v1/redirect/239f60a8-9731-4428-8e3a-6e3e7d81a52b/express/?preference-id=192393632-2ee2a8f9-dbd8-4309-a0be-afec17a95480');
             }
             break;
         case '2':
@@ -43,10 +41,8 @@ $(".select-convites").on('change', function (e) {
         case '1':
             if (checkbox == 'nao') {
                 $('.valor-plano-basico').html('R$ 419');
-                $(`.botao-comprar-basico`).attr('href', 'https://www.mercadopago.com.br/checkout/v1/redirect/66dbc52d-6efc-42c5-8e05-d33ef1619e16/express/?preference-id=192393632-6c833960-c398-4aee-91b0-65daa3a25ba8');
             } else {
                 $('.valor-plano-basico').html('R$ 499');
-                $(`.botao-comprar-basico`).attr('href', 'https://www.mercadopago.com.br/checkout/v1/redirect/239f60a8-9731-4428-8e3a-6e3e7d81a52b/express/?preference-id=192393632-2ee2a8f9-dbd8-4309-a0be-afec17a95480');
             }
             break;
         case '2':
@@ -74,11 +70,9 @@ $(".select-pro").on('change', function (e) {
 
         case '1':
                 $('.valor-plano-pro').html('R$ 1.320');
-                $(`.botao-comprar-pro`).attr('href', 'https://www.mercadopago.com.br/checkout/v1/redirect/66dbc52d-6efc-42c5-8e05-d33ef1619e16/express/?preference-id=192393632-6c833960-c398-4aee-91b0-65daa3a25ba8');
             break;
         case '2':
                 $('.valor-plano-pro').html('R$ 1.700');
-                $(`.botao-comprar-pro`).attr('href', 'https://www.mercadopago.com.br/checkout/v1/redirect/66dbc52d-6efc-42c5-8e05-d33ef1619e16/express/?preference-id=192393632-6c833960-c398-4aee-91b0-65daa3a25ba8');
             break;
     }
 });
@@ -96,11 +90,9 @@ $(".select-ultra").on('change', function (e) {
 
         case '1':
                 $('.valor-plano-ultra').html('R$ 2.000');
-                $('.botao-comprar-ultra').attr('href', 'https://www.mercadopago.com.br/checkout/v1/redirect/adf2ab9a-9cb1-4157-93f7-33088cd883ab/express/?preference-id=192393632-2ba12644-31cd-4d89-8615-122a24f3ea4c');
             break;
         case '2':
                 $('.valor-plano-ultra').html('R$ 2.500');
-                $('.botao-comprar-ultra').attr('href', 'https://www.mercadopago.com.br/checkout/v1/redirect/99ecd986-76c5-4546-ab26-d5120d4d672b/express/?preference-id=192393632-1b7c1df3-8621-4e62-8de2-d0dd48f91055');
             break;
     }
 });
--- php/comprar.php
+++ php/comprar.php
@@ -4,6 +4,8 @@ ini_set('display_errors', 1);
 
 error_reporting(E_ALL);
 
+if (!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['phone']) && !empty($_POST['subject']) && !empty($_POST['planos']) && !empty($_POST['endereco']) && !empty($_POST['fpagamento']) && !empty($_POST['message'])) {
+    
 $nome = $_POST['name'];
 $email = $_POST['email'];
 $telefone = $_POST['phone'];
@@ -13,9 +15,9 @@ $endereco = $_POST['endereco'];
 $fpagamento = $_POST['fpagamento'];
 $mensagem = $_POST['message'];
 $to = "contato@acquacerrado.com.br";
- 
-
-$message = "Email: ".$email."<br/>"."Nome: ".$nome."<br/>"."Telefone: ".$telefone."<br/>"."Assunto: 
+    
+    
+    $message = "Email: ".$email."<br/>"."Nome: ".$nome."<br/>"."Telefone: ".$telefone."<br/>"."Assunto: 
 ".$assunto."<br/>"."Mensagem: ".$mensagem."<br/>"."Plano: ".$plano."<br/>"."Endereço: ".$endereco."<br/>"."Forma de Pagamento: ".$fpagamento."<br/>";
 
 
@@ -25,4 +27,9 @@ $headers = 'Content-type: text/html;' . "\r\n";
 mail($to, $assunto, $message, $headers);
 
 header("Location: ../index.html");
+
+} else {
+    header("Location: ../404.html");
+}
+
 ?>
