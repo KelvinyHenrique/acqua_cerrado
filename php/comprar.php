ba4f607c74b6c2cc54c5f19fd9468329b8ff3d11
kelvinyhenrique <kelvinyhenrique17@gmail.com>
2020-01-23 13:00:24 -0300

    Ajustando Formulario de Contato

--- php/comprar.php
+++ php/comprar.php
@@ -4,21 +4,19 @@ ini_set('display_errors', 1);
 
 error_reporting(E_ALL);
 
-if (!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['phone']) && !empty($_POST['subject']) && !empty($_POST['planos']) && !empty($_POST['endereco']) && !empty($_POST['fpagamento']) && !empty($_POST['message'])) {
+if (!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['phone']) && !empty($_POST['subject']) && !empty($_POST['endereco']) && !empty($_POST['message'])) {
     
 $nome = $_POST['name'];
 $email = $_POST['email'];
 $telefone = $_POST['phone'];
 $assunto = $_POST['subject'];
-$plano = $_POST['planos'];
 $endereco = $_POST['endereco'];
-$fpagamento = $_POST['fpagamento'];
 $mensagem = $_POST['message'];
 $to = "contato@acquacerrado.com.br";
     
     
     $message = "Email: ".$email."<br/>"."Nome: ".$nome."<br/>"."Telefone: ".$telefone."<br/>"."Assunto: 
-".$assunto."<br/>"."Mensagem: ".$mensagem."<br/>"."Plano: ".$plano."<br/>"."Endereço: ".$endereco."<br/>"."Forma de Pagamento: ".$fpagamento."<br/>";
+".$assunto."<br/>"."Mensagem: ".$mensagem."<br/>"."Endereço: ".$endereco."<br/>";
 
 
 $headers = "De:". $email;
